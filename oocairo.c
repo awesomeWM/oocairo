49736f13b2b0b9ebfb3cf57eb8fe34b1762d192f
Uli Schlachter <psychon@znc.in>
2010-08-22 16:44:46 +0200

    Make the IDs of the meta-tables visible
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- oocairo.c
+++ oocairo.c
@@ -19,15 +19,6 @@
 #error "This Lua binding requires Cairo version 1.6 or better."
 #endif
 
-#define MT_NAME_CONTEXT    ("6404c570-6711-11dd-b66f-00e081225ce5")
-#define MT_NAME_FONTFACE   ("ee272774-6a1e-11dd-86de-00e081225ce5")
-#define MT_NAME_FONTOPT    ("8ae95550-9887-11dd-922a-00e081225ce5")
-#define MT_NAME_MATRIX     ("6e2f4c64-6711-11dd-acfc-00e081225ce5")
-#define MT_NAME_PATH       ("6d83bf34-6711-11dd-b4c2-00e081225ce5")
-#define MT_NAME_PATTERN    ("6dd49a26-6711-11dd-88fd-00e081225ce5")
-#define MT_NAME_SCALEDFONT ("b8012f94-98b0-11dd-b174-00e081225ce5")
-#define MT_NAME_SURFACE    ("6d31a064-6711-11dd-bdd8-00e081225ce5")
-
 static const int ENDIANNESS_TEST_VAL = 1;
 #define IS_BIG_ENDIAN (!(*(const char *) &ENDIANNESS_TEST_VAL))
 
